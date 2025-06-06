983ed3d03d046b159e48e45f0e5456556581cc7e
Vladislav <gogoha.hug.8172@gmail.com>
2025-06-06 20:50:14 +0300

    controller + services

--- src/main/resources/db/migration/V1__create_entity.sql
+++ src/main/resources/db/migration/V1__create_entity.sql
@@ -1,35 +1,37 @@
-CREATE SCHEMA IF EXISTS user_schema;
+CREATE SCHEMA IF NOT EXISTS users_schema;
 
-CREATE TABLE IF NOT EXISTS t_users(
-    id SERIAL PRIMARY KEY NOT NULL,
-    c_name VARCHAR(50) NOT NULL,
-    c_age INTEGER NOT NULL CHECK (c_age > 0 AND c_age < 150),
-    c_passport_id INTEGER UNIQUE,
-    FOREIGN KEY (c_passport_id) REFERENCES t_passports(id)
-);
-
-CREATE TABLE IF NOT EXISTS t_passports(
+CREATE TABLE IF NOT EXISTS users_schema.t_passports(
     id SERIAL PRIMARY KEY NOT NULL,
     c_number VARCHAR(12) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS t_accounts(
+-- Then create users
+CREATE TABLE IF NOT EXISTS users_schema.t_users(
     id SERIAL PRIMARY KEY NOT NULL,
-    c_title VARCHAR(50) NOT NULL,
-    c_user_id  INTEGER,
-    FOREIGN KEY (c_user_id) REFERENCES t_users(id)
+    c_name VARCHAR(50) NOT NULL,
+    c_age INTEGER NOT NULL CHECK (c_age > 0 AND c_age < 150),
+    c_passport_id INTEGER UNIQUE,
+    FOREIGN KEY (c_passport_id) REFERENCES users_schema.t_passports(id)
 );
 
-CREATE TABLE IF NOT EXISTS t_hobbies(
+-- Then other tables
+CREATE TABLE IF NOT EXISTS users_schema.t_accounts(
+    id SERIAL PRIMARY KEY NOT NULL,
+    c_title VARCHAR(50) NOT NULL,
+    c_user_id INTEGER,
+    FOREIGN KEY (c_user_id) REFERENCES users_schema.t_users(id)
+);
+
+CREATE TABLE IF NOT EXISTS users_schema.t_hobbies(
     id SERIAL PRIMARY KEY NOT NULL,
     c_type VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS t_users_hobbies(
-    c_user_id SERIAL,
-    c_hobby_id SERIAL,
+-- Junction table
+CREATE TABLE IF NOT EXISTS users_schema.t_users_hobbies(
+    c_user_id INTEGER NOT NULL,
+    c_hobby_id INTEGER NOT NULL,
     PRIMARY KEY (c_user_id, c_hobby_id),
-    FOREIGN KEY (c_user_id) REFERENCES  t_users(id),
-    FOREIGN KEY(c_hobby_id) REFERENCES t_hobbies(id)
-
+    FOREIGN KEY (c_user_id) REFERENCES users_schema.t_users(id),
+    FOREIGN KEY (c_hobby_id) REFERENCES users_schema.t_hobbies(id)
 );
