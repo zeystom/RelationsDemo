ed060e28861167eed9e1bfe97e0c256f34f5905c
Vladislav <gogoha.hug.8172@gmail.com>
2025-06-11 19:31:41 +0300

    controller + services

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,5 @@
 package com.example.demo.exceptions;
 
-import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,27 +12,14 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @Operation(
-            summary = "Handle validation errors",
-            description = "Triggered when request data fails validation (e.g., empty fields)"
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Bad Request: Invalid input data",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = Map.class),
-                            examples = @ExampleObject(
-                                    value = "{\"username\": \"Username is required\", \"email\": \"Invalid email format\"}"
-                            )
-                    )
-            )
-    })
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    public  ResponseEntity<Map<String, String>> handleNotFoundErrors(NoSuchElementException ex){
+        Map<String,String> errors = Map.of("messgae", ex.getMessage()   );
+        return ResponseEntity.status(404).body(errors);
     }
 }
