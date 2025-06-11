4e75193cf315cbc3b9d8a79d4f47c02af4e35abf
Vladislav <gogoha.hug.8172@gmail.com>
2025-06-11 19:14:36 +0300

    controller + services

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,25 +1,25 @@
-package com.example.demo.exceptions;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex){
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
-        return ResponseEntity.badRequest().body(errors);
-    }
-    @ExceptionHandler(NoSuchElementException.class)
-    public  ResponseEntity<Map<String, String>> handleNotFoundErrors(NoSuchElementException ex){
-        Map<String,String> errors = Map.of("messgae", ex.getMessage()   );
-        return ResponseEntity.status(404).body(errors);
-    }
-}
+//package com.example.demo.exceptions;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.NoSuchElementException;
+//
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex){
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+//        return ResponseEntity.badRequest().body(errors);
+//    }
+//    @ExceptionHandler(NoSuchElementException.class)
+//    public  ResponseEntity<Map<String, String>> handleNotFoundErrors(NoSuchElementException ex){
+//        Map<String,String> errors = Map.of("messgae", ex.getMessage()   );
+//        return ResponseEntity.status(404).body(errors);
+//    }
+//}
