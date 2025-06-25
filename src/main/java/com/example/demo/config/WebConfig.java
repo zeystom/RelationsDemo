21a937999d2f0b813ba8ad2262fb6473717886b8
Vladislav <gogoha.hug.8172@gmail.com>
2025-06-25 20:28:12 +0300

    web

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -2,9 +2,11 @@ package com.example.demo.config;
 
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
